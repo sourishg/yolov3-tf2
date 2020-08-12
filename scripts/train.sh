b98cc62c02b7f610fbca8cd493752f0e0964171f
Sourish Ghosh <ragnarok0211@gmail.com>
2020-08-12 07:21:45 +0000

    small fixes

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 export TF_FORCE_GPU_ALLOW_GROWTH=true
 
-TRAIN_DATASET='/home/sourish/dev/flying_object_detection/fod/dataengine/data/xplane/train/mount_morning_cirrus_1_temp3.tfrecord'
-VAL_DATASET='/home/sourish/dev/flying_object_detection/fod/dataengine/data/xplane/test/mount_evening_clear_2_temp.tfrecord'
+TRAIN_DATASET='/mnt/datasets/xplane/train/*.tfrecord'
+VAL_DATASET='/mnt/datasets/xplane/test/*.tfrecord'
 WEIGHTS='./checkpoints/yolov3.tf'
 IMG_SIZE=1280
 EPOCHS=30
-BATCH_SIZE=1
+BATCH_SIZE=16
 NUM_CLASSES=11
 LEARNING_RATE=1e-3
 MODE='fit'
@@ -22,4 +22,5 @@ python train.py \
 --learning_rate=$LEARNING_RATE \
 --size=$IMG_SIZE \
 --mode=$MODE \
---transfer=$TRANSFER
+--transfer=$TRANSFER \
+--tiny
